60b6a820e27991832d87bf8f20aea30cbc8a4e3a
TriBhaskar <panthribhaskar@gmail.com>
2024-10-08 22:34:06 +0530

    Remove total row from Content component and adjust Footer component layout

--- src/components/layouts/content/Content.tsx
+++ src/components/layouts/content/Content.tsx
@@ -65,12 +65,7 @@ export default function Content() {
             </TableRow>
           ))}
         </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
-        </TableFooter>
+        <TableFooter></TableFooter>
       </Table>
     </>
   );
